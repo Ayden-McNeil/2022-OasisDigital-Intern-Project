dd5c25933c705a88516f9e391358daf90d53b0ad
ManuelPaurevic <34079382+ManuelPaurevic@users.noreply.github.com>
2022-06-01 13:56:45 -0500

    Updated UI and created 2 Functions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour{
+
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI timerText;
+
+    private int score = 0;
+    public float time = 30;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText.text = score.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Timer();
+    }
+
+    public void ScoreKeeper(int pointsAdded){
+        score += pointsAdded;
+        scoreText.text = score.ToString();
+    }
+
+    private void Timer(){
+        
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+            timerText.text = Mathf.RoundToInt(time).ToString();
+        }
         
     }
+
+
+
+
+
+
 }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     [SerializeField] private float lifeTime;
+    [SerializeField] private int pointValue;
     static private SpawnManager spawnManagerScript;
+    static private GameManager gameManagerScript;
 
     private void Awake()
     {
         spawnManagerScript = FindObjectOfType<SpawnManager>();
+        gameManagerScript = FindObjectOfType<GameManager>();
     }
 
     private void OnMouseDown()
     {
         spawnManagerScript.SpawnTargets();
         spawnManagerScript.RemovePostionFromList(transform.position);
+        gameManagerScript.ScoreKeeper(pointValue);
         Destroy(gameObject);
-
     }
 }
