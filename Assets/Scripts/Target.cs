f789b101946a9fc0642f5584c2108426fc5608fd
ManuelPaurevic <34079382+ManuelPaurevic@users.noreply.github.com>
2022-06-06 13:17:31 -0500

    Full created Particle System

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -34,7 +34,6 @@ public class Target : MonoBehaviour
             gameManagerScript.ScoreKeeper(pointValue);
             Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
             numberOfTargetsDestroyed++;
-
         }
     }
 
