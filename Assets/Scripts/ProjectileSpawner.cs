d90a95fd90dd5f1d243826dcd6e7b7c3e7c7fe5f
ManuelPaurevic <34079382+ManuelPaurevic@users.noreply.github.com>
2022-06-07 13:23:28 -0500

    Small fixes

--- Assets/Scripts/ProjectileSpawner.cs
+++ Assets/Scripts/ProjectileSpawner.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class ProjectileSpawner : MonoBehaviour
 {
     public GameObject projectile;                       //Projectile Prefab
-    [SerializeField] private Camera firstPersonCamera;  //Camera Gameobject
+    [SerializeField] private Camera firstPersonCamera;  //First person Camera Gameobject
+    [SerializeField] private Camera thirdPersonCamera;  //Third person Camera Gameobject
+    [SerializeField] private Camera currentCamera;      //Selected Camera Gameobject
     [SerializeField] private float distance = 1;        //Distance infront of the camera
     static private GameManager gameManagerScript;
+    static public int CameraPov;
 
     private void Start()
     {
+        CameraPov = sceneVarPassover.pov;
         gameManagerScript = FindObjectOfType<GameManager>();
+        if(CameraPov == 3){
+            currentCamera = thirdPersonCamera;
+        }else{
+            currentCamera = firstPersonCamera;
+        }
     }
 
     void Update()
@@ -25,8 +34,8 @@ public class ProjectileSpawner : MonoBehaviour
     //Gets the postion of the camera and infront of it 
     public void SpawnProjectile(){
        
-        Vector3 spawnPosition = firstPersonCamera.transform.position + firstPersonCamera.transform.forward * distance;
-        Instantiate(projectile, spawnPosition, firstPersonCamera.transform.rotation);
+        Vector3 spawnPosition = currentCamera.transform.position + currentCamera.transform.forward * distance;
+        Instantiate(projectile, spawnPosition, currentCamera.transform.rotation);
     }
 
 
