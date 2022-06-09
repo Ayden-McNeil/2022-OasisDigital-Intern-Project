3f3c7cb1cd8bac0c43ecfd0816682b3269510d4c
Adam-McNeil <adam.mcneil@oasisdigital.com>
2022-06-09 10:18:59 -0500

    fixed player controller bug where the player rotated and added ray casting system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,10 +9,12 @@ public class PlayerController : MonoBehaviour
     private float yMouse;
     private float xRotation;
     private float yRotation;
+    
     [SerializeField] private float xSensitivity = 10f;
     [SerializeField] private float ySensitivity = 10f;
     public Camera playerCameraFP;           //First Person Camera
     public Camera playerCameraTP;           //Third Person Camera
+    private Camera mainCamera;
     public float sensVar;
     public int fovVar;
     public int povVar;
@@ -22,34 +24,46 @@ public class PlayerController : MonoBehaviour
     private int lastZInput = 0;
     public float speed = 10.0f;
 
+    private bool isFirstPerson = false;
+    private bool isThirdPerson = false;
+
+    private Transform focalTransform;
+
+    [SerializeField] private GameObject pointer;
+    [SerializeField] private LayerMask layerMask;
+
     private void Start()
     {
-        
         body = GetComponent<Rigidbody>();
+        focalTransform = GameObject.Find("FocalPoint").GetComponent<Transform>();
         Cursor.lockState = CursorLockMode.Locked;
+
         sensVar = sceneVarPassover.sens;
         fovVar = sceneVarPassover.fov;
         povVar = sceneVarPassover.pov;
-        if(povVar == 3){
-            playerCameraTP.fieldOfView = fovVar;
-            playerCameraTP.gameObject.SetActive(true);
-        }else{
-            playerCameraFP.fieldOfView = fovVar;
-            playerCameraFP.gameObject.SetActive(true);
+
+        if(povVar == 3)
+        {
+            mainCamera = playerCameraTP;
+            isThirdPerson = true;
         }
+        else
+        {
+            mainCamera = playerCameraFP;
+            isFirstPerson = true;
+
+        }
+        mainCamera.fieldOfView = fovVar;
+        mainCamera.gameObject.SetActive(true);
+        
     }
 
     private void Update()
     {
-        xMouse = Input.GetAxis("Mouse X") * Time.deltaTime * xSensitivity * sensVar;
-        yMouse = Input.GetAxis("Mouse Y") * Time.deltaTime * ySensitivity * sensVar;
+       DoRayCast();
+       RotateCamera();
+       Move();
         
-        xRotation -= yMouse;
-        yRotation += xMouse;
-        xRotation = Mathf.Clamp(xRotation, -90, 90);
-
-        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0);
-        Move();
     }
 
     void Move()
@@ -88,5 +102,37 @@ public class PlayerController : MonoBehaviour
         Vector3 moveVector = (transform.forward * zInput * Time.deltaTime + transform.right * xInput * Time.deltaTime) * speed;
         moveVector.y = body.velocity.y;
         body.velocity = moveVector;
+    }
+
+    void RotateCamera()
+    {
+        xMouse = Input.GetAxis("Mouse X") * Time.deltaTime * xSensitivity * sensVar;
+        yMouse = Input.GetAxis("Mouse Y") * Time.deltaTime * ySensitivity * sensVar;
+
+        yRotation += xMouse;
+        xRotation -= yMouse;
+
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(0, yRotation, 0);
+        if (isFirstPerson)
+        {
+            mainCamera.gameObject.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0);
+        }
+        if (isThirdPerson)
+        {
+            focalTransform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        }
+    }
+
+    void DoRayCast()
+    {
+        Vector2 centerScreenPosition = new Vector2(Screen.width / 2, Screen.height / 2);
+        Ray ray = mainCamera.ScreenPointToRay(centerScreenPosition);
+        if (Physics.Raycast(ray, out RaycastHit rayCastHit, float.MaxValue, layerMask))
+        {
+            pointer.transform.position = rayCastHit.point;
+        }
     }
 }
