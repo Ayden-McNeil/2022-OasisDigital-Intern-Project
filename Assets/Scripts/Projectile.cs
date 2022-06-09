f8b6f71304e281922c0b5ebbeada0e6a1839857a
ManuelPaurevic <34079382+ManuelPaurevic@users.noreply.github.com>
2022-06-09 15:41:39 -0500

    Projectiles Have Different Colors

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,13 +6,13 @@ public class Projectile : MonoBehaviour{
     // Start is called before the first frame update
 
     private Rigidbody rb;                            //GameComponent RigidBody
-    [SerializeField] private float speed = 100;       //Strength of the shot
+    [SerializeField] private float speed = 100;      //Strength of the shot
     [SerializeField] private float lifeTime = 1;     //How long the gameobject lasts
-
-
+     
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();             
+        gameObject.GetComponent<Renderer>().material.color = RandomColor();
         rb.velocity = transform.forward * speed;
         Destroy(gameObject, lifeTime);
     }
@@ -23,4 +23,14 @@ public class Projectile : MonoBehaviour{
             Destroy(other.gameObject);
         }
     }
+
+    private Color RandomColor(){
+        int r,b,g;
+        r = Random.Range(40, 255);
+        b = Random.Range(40, 255);
+        g = Random.Range(40, 255);
+        Color projectileColor = new Color32((byte)r,(byte)b,(byte)g, 1);
+        return projectileColor;
+    }
 }
+
