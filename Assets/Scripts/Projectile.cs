a8c4f3ba5a5dc0505085967a148c65cf2d9ca562
Adam-McNeil <adam.mcneil@oasisdigital.com>
2022-06-21 09:23:23 -0500

    test sound fix

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Projectile : MonoBehaviour{
 
     [SerializeField] private float lifeTime = 5;     //How long the gameobject lasts
-     
+    [SerializeField] private AudioClip hitSound;
+    [SerializeField] private AudioClip shootSound;
+    private AudioSource audioSource;
+
     void Start()
     {
         gameObject.GetComponent<Renderer>().material.color = RandomColor();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(shootSound);
         Destroy(gameObject, lifeTime);
     }
 
@@ -16,6 +21,7 @@ public class Projectile : MonoBehaviour{
         if(other.gameObject.tag == "Target")
         {
             Destroy(other.gameObject);
+            audioSource.PlayOneShot(hitSound);
         }
     }
 
